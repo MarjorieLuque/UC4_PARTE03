15442c6956db6a986188a8f74b35ca79e28c00ea
Jten202 <jtenorio202@gmail.com>
2023-12-11 09:10:42 -0500

    CRUD CURSOS - Listo

--- mi_aplicacion/views.py
+++ mi_aplicacion/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Course
 
 def cursos(request):
-    return render(request, 'cursos.html', {})
+    cursosListado = Course.objects.all()
+    return render(request, 'cursos.html', {"curso": cursosListado})
 
 def crear_curso(request):
     return render(request, 'crear_curso.html', {})
@@ -18,4 +20,16 @@ def index(request):
     return render(request, 'index.html')  # Reemplaza 'index.html' con el nombre de tu plantilla
 # mi_aplicacion/views.py
 
-from django.shortcuts import render
+def eliminar_curso(request,idcourse):
+    curso=Course.objects.get(idcourse=idcourse)
+    curso.delete()
+    return redirect('/cursos/')
+
+def registrar_curso(request):
+    code=request.POST['code']
+    name=request.POST['name']
+    hour=request.POST['hour']
+    credits=request.POST['credits']
+    state=request.POST['state']
+    curso=Course.objects.create(code=code,name=name,hour=hour,credits=credits,state=state)
+    return redirect('/crear_curso/')
